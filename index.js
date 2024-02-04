1c07e11a6b3bf19adf99f4a1b393a0b76c771270
Jamie Thomson <156948235+jamieOthomson@users.noreply.github.com>
2024-02-04 19:48:26 +0000

    Arrays completed

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-let firstCard = 7
-let secondCard = 10
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+function getRandomCard() {
+  let randomNumber =  Math.floor( Math.random()*13 ) + 1
+  if (randomNumber === 1) {
+      return 11
+  } else if (randomNumber > 10) {
+      return 10
+  } else {
+      return randomNumber
+  }
+}
+
 function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
   renderGame()
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+  cardsEl.textContent = "Cards: "
+  for (i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
   sumEl.textContent = "Sum: " + sum
   if (sum < 21) {
     message = "Do you want another card?"
@@ -31,7 +48,10 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 4
-  sum += card
-  renderGame()
+  if (isAlive === true && hasBlackjack === false) {
+    let card = getRandomCard()
+    cards.push(card)
+    sum += card
+    renderGame()
+  }
 }
